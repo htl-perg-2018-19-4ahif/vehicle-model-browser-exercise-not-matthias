a011072aab46e04a10d10c0085987c688557d233
not-matthias <26800596+not-matthias@users.noreply.github.com>
2019-04-25 14:54:11 +0200

    Added about page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,16 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { VehicleApiService } from '../vehicle-api.service';
+import { LoremIpsum } from 'lorem-ipsum';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent {
+  public lorem = new LoremIpsum({
+    sentencesPerParagraph: {
+      max: 8,
+      min: 4
+    },
+    wordsPerSentence: {
+      max: 16,
+      min: 4
+    },
+    format: 'html'
+  });
+  public aboutText = '';
 
-  constructor(private api: VehicleApiService) { }
-
-  ngOnInit() {
+  constructor(private api: VehicleApiService) {
+    this.aboutText = this.lorem.generateParagraphs(7);
   }
-
 }
